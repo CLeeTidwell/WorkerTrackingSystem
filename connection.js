187a063b6981c27f1de03b408f78b8ca779d222a
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-06-01 13:55:41 -0700

    fixed rando shtuff

--- connection.js
+++ connection.js
@@ -1,17 +1,19 @@
 const mysql = require('mysql2');
 
-const join = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Kirin@1988',
-    database: 'wts_db'
-});
+const db = mysql.createConnection(
+    {
+     host: 'localhost',
+     user: 'root',
+     password: 'Kirin@1988',
+     database: 'wts_db'
+    }
+);
 
-join.connect(function (err) {
+db.connect(function (err) {
     if (err) {
         throw err;
     } else {
         console.log('\nConnection Success!');
 }});
 
-module.exports = join;
+module.exports = db;
